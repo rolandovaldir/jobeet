89bd21412db3f2ac0e2fc1d65c9e15d93154206d
rolandovaldir <valdir.cadima@gmail.com>
2015-11-05 17:22:38 -0400

    Dia 5

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -11,5 +11,27 @@
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
 class JobeetCategory extends BaseJobeetCategory
-{
+{  
+    
+  public function countActiveJobs()
+  {
+    return $this->getActiveJobsQuery()->count();
+  }
+  
+  public function getActiveJobs($max = 10)
+  {
+    $q = $this->getActiveJobsQuery()
+      ->limit($max);
+   
+    return $q->execute();
+  }
+  
+  public function getActiveJobsQuery()
+  {
+    $q = Doctrine_Query::create()
+      ->from('JobeetJob j')
+      ->where('j.category_id = ?', $this->getId());
+   
+    return Doctrine_Core::getTable('JobeetJob')->addActiveJobsQuery($q);
+  }
 }
